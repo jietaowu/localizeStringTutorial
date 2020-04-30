ff9f142c692e7a47e010a470b0680b603845211d
jietaowu <jietaowu2014@gmail.com>
2020-04-30 18:39:01 -0400

    update

--- localizeString/ViewController.swift
+++ localizeString/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        label.text = NSLocalizedString("please subscribe", comment: "")
+       
+        
     }
 
 
