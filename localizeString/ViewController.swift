8508c37bf20713d434cbe1f08c12906a4496117f
jietaowu <jietaowu2014@gmail.com>
2020-04-30 18:09:30 -0400

    Initial Commit

---
+++ localizeString/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  localizeString
+//
+//  Created by jwu on 4/30/20.
+//  Copyright © 2020 jwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
